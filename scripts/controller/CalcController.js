92856cbbcc143476815ce506e49a08a335702b35
gabrielvieiracode <gabriel.vieira.trabalho@gmail.com>
2020-04-18 05:01:21 -0300

    Criaçao dos calculos e do display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -35,10 +35,10 @@ class CalcController {
     }
 
     getLastOperation() {
-       return this._operation[this._operation.length - 1];
+        return this._operation[this._operation.length - 1];
     }
 
-    setLastOperation(value){
+    setLastOperation(value) {
         this._operation[this._operation.length - 1] = value;
 
     }
@@ -46,9 +46,38 @@ class CalcController {
         return (['+', '-', '/', '*', '%'].indexOf(value) > -1);
 
     }
+    pushOperation(value){
+        this._operation.push(value);
+
+        if(this._operation.length>3){
+            this.calc();
+            
+        }
+    }
+
+    calc(){
+        let last = this._operation.pop();
+       let result = eval(this._operation.join(''));
+
+       this._operation = [result,last]
+       this.setLastNumberToDisplay();
+
+    }
+
+    setLastNumberToDisplay(){
+        let lastNumber ;
+        for(let i = this._operation.length -1; i>=0 ;i--){
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+
+                break;
+            }
+        }
+        this.displayCalc = lastNumber
+    }
 
     addOperation(value) {
-            console.log('a',isNaN(this.getLastOperation()));
+
         if (isNaN(this.getLastOperation())) {
             if (this.isOperator(value)) {
                 this.setLastOperation(value);
@@ -56,20 +85,29 @@ class CalcController {
             }
             else if (isNaN(value)) {
                 //Outracoisa
-                console.log(value)
+                console.log('Outra coisa',value)
 
             } else {
-                this._operation.push(value);
+
+                this.pushOperation(value);
+                this.setLastNumberToDisplay()
+
             }
         }
 
         else {
-            let newValue = this.getLastOperation().toString() + value.toString()
-            this.setLastOperation(parseInt(newValue));
-        }
+            if (this.isOperator(value)) {
+                this.pushOperation(value)
+            }
+            else {
+                let newValue = this.getLastOperation().toString() + value.toString()
+                this.setLastOperation(parseInt(newValue));
 
-        // this._operation.push(value)
-        console.log(this._operation)
+                //Atualizar display
+                this.setLastNumberToDisplay()
+
+            }
+        }
     }
 
     setError() {
@@ -90,7 +128,7 @@ class CalcController {
                 this.addOperation('+');
                 break;
 
-            case 'porcento ':
+            case 'porcento':
                 this.addOperation('%');
 
                 break;
