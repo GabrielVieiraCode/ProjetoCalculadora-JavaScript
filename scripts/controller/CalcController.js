55c93294ba4e7a2dc7977abd98c12cff24652119
gabrielvieiracode <gabriel.vieira.trabalho@gmail.com>
2020-04-19 20:26:43 -0300

    Criaçao dos metodos de declado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -10,6 +10,7 @@ class CalcController {
         this._currentDate;
         this.iniatilze();
         this.initButtonsEvents();
+        this.initKeyBoard();
 
     }
 
@@ -20,10 +21,75 @@ class CalcController {
 
         }, 1000);
         this.setLastNumberToDisplay();
-
+        this.pasteFromClipBoard();
     }
-    initKeyBoard(){
-        
+
+
+    pasteFromClipBoard() {
+        document.addEventListener('paste', e => {
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc = parseFloat(text);
+        })
+    }
+    copyToClipBoard() {
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc
+
+        document.body.appendChild(input);
+        input.select();
+
+        document.execCommand("Copy")
+
+        input.remove();
+    }
+    initKeyBoard() {
+        document.addEventListener('keyup', e => {
+            switch (e.key) {
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry()
+                    break;
+
+                case '+':
+                case '%':
+                case '*':
+                case '-':
+                case '/':
+                    this.addOperation(e.key);
+                    break;
+
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                case '.':
+                case ',':
+                    this.addDot('.');
+
+                    break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipBoard()
+                    break;
+
+
+            }
+        })
     }
 
     addEventListenerAll(element, events, fn) {
@@ -33,7 +99,7 @@ class CalcController {
     }
     clearAll() {
         this._operation = [];
-        this._lastNumber ='';
+        this._lastNumber = '';
         this._lastOperator = '';
         this.setLastNumberToDisplay();
 
@@ -75,9 +141,9 @@ class CalcController {
         let last = ''
 
         this._lastOperator = this.getLastItem();
-        if(this._operation.length <3){
+        if (this._operation.length < 3) {
             let firstItem = this._operation[0];
-            this._operation = [firstItem,this._lastOperator , this._lastNumber]
+            this._operation = [firstItem, this._lastOperator, this._lastNumber]
         }
 
         if (this._operation.length > 3) {
@@ -87,7 +153,7 @@ class CalcController {
         else if (this._operation.length == 3) {
             this._lastNumber = this.getLastItem(false);
         }
-        
+
         let result = this.getResult();
 
         if (last == '%') {
@@ -116,8 +182,8 @@ class CalcController {
 
                 break;
             }
-            if(!lastItem){
-                lastItem =(isOperator)? this._lastOperator: this._lastNumber;
+            if (!lastItem) {
+                lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
             }
 
         }
@@ -164,15 +230,15 @@ class CalcController {
     setError() {
         this.displayCalc = "Error";
     }
-    addDot(){
-        let lastOperation=this.getLastOperation();
-        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.')>-1)return;
+    addDot() {
+        let lastOperation = this.getLastOperation();
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
-        if(this.isOperator(lastOperation)|| !lastOperation){
+        if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
         }
-        else{
-            this.setLastOperation(lastOperation.toString()+ '.')
+        else {
+            this.setLastOperation(lastOperation.toString() + '.')
         }
         this.setLastNumberToDisplay()
     }
