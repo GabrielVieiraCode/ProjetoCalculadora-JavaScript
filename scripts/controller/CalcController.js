2012d65f289ac95b427860a441ebdf5dff112829
gabrielvieiracode <gabriel.vieira.trabalho@gmail.com>
2020-04-18 21:17:26 -0300

    Criaçao dos metodos igual e bug do click duplo

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor() {
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
@@ -17,7 +19,8 @@ class CalcController {
             this.setDisplayDateTime();
 
         }, 1000);
-        ;
+        this.setLastNumberToDisplay();
+
     }
 
     addEventListenerAll(element, events, fn) {
@@ -27,11 +30,17 @@ class CalcController {
     }
     clearAll() {
         this._operation = [];
+
+        this.setLastNumberToDisplay();
+
     }
 
     clearEntry() {
 
         this._operation.pop()
+
+        this.setLastNumberToDisplay();
+
     }
 
     getLastOperation() {
@@ -46,33 +55,75 @@ class CalcController {
         return (['+', '-', '/', '*', '%'].indexOf(value) > -1);
 
     }
-    pushOperation(value){
+    pushOperation(value) {
         this._operation.push(value);
 
-        if(this._operation.length>3){
+        if (this._operation.length > 3) {
             this.calc();
-            
+
         }
     }
-
-    calc(){
-        let last = this._operation.pop();
-       let result = eval(this._operation.join(''));
-
-       this._operation = [result,last]
-       this.setLastNumberToDisplay();
-
+    getResult() {
+        return eval(this._operation.join(""));
     }
 
-    setLastNumberToDisplay(){
-        let lastNumber ;
-        for(let i = this._operation.length -1; i>=0 ;i--){
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i];
+    calc() {
+        let last = ''
+
+        this._lastOperator = this.getLastItem();
+        if(this._operation.length <3){
+            let firstItem = this._operation[0];
+            this._operation = [firstItem,this._lastOperator , this._lastNumber]
+        }
+
+        if (this._operation.length > 3) {
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+        }
+        else if (this._operation.length == 3) {
+            this._lastNumber = this.getLastItem(false);
+        }
+        
+        let result = this.getResult();
+
+        if (last == '%') {
+            result /= 100;
+            this._operation = [result]
+
+        }
+        else {
+
+            this._operation = [result]
+            if (last) this._operation.push(last)
+        }
+
+
+
+        this.setLastNumberToDisplay();
+
+    }
+    getLastItem(isOperator = true) {
+        let lastItem;
+
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                lastItem = this._operation[i];
 
                 break;
             }
+            if(!lastItem){
+                lastItem =(isOperator)? this._lastOperator: this._lastNumber;
+            }
+
         }
+        return lastItem
+    }
+
+    setLastNumberToDisplay() {
+        let lastNumber = this.getLastItem(false);
+
+        if (!lastNumber) lastNumber = 0;
         this.displayCalc = lastNumber
     }
 
@@ -85,7 +136,7 @@ class CalcController {
             }
             else if (isNaN(value)) {
                 //Outracoisa
-                console.log('Outra coisa',value)
+                console.log('Outra coisa', value)
 
             } else {
 
@@ -121,7 +172,7 @@ class CalcController {
                 this.clearAll();
                 break;
             case 'ce':
-                this.clearAll()
+                this.clearEntry()
                 break;
 
             case 'soma':
@@ -149,7 +200,7 @@ class CalcController {
                 break;
 
             case 'igual':
-
+                this.calc();
                 break;
             case 'ponto':
                 this.addOperation('.');
